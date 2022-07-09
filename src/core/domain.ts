8aaf56c5a8d98c73b7f8abb2a5d1250f04f2c1bd
Alexander Bespoyasov <forjob159@gmail.com>
2022-07-09 18:55:29 +0200

    Expose representation types

--- src/core/domain.ts
+++ src/core/domain.ts
@@ -2,11 +2,11 @@ type MilliSecondsNumber = number;
 type SecondsNumber = number;
 type DateIsoString = string;
 
-type StringRepresentation = DateIsoString;
-type NumberRepresentation = SecondsNumber | MilliSecondsNumber;
+export type Unit = "ms" | "s";
 
 export type TimeStamp = MilliSecondsNumber;
-export type Unit = "ms" | "s";
+export type StringRepresentation = DateIsoString;
+export type NumberRepresentation = SecondsNumber | MilliSecondsNumber;
 
 export type Normalizer = (unit: Unit, value: NumberRepresentation) => TimeStamp;
 export type Projector = (unit: Unit, value: TimeStamp) => NumberRepresentation;
