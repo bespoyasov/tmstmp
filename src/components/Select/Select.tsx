28d37ba383e52576b3a29566c51a85b521458edd
Alexander Bespoyasov <forjob159@gmail.com>
2022-07-09 13:18:25 +0200

    Add label for the select

--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -1,10 +1,15 @@
+import { VisuallyHidden } from "../VisuallyHidden";
 import styles from "./Select.module.css";
 
 export const Select = () => {
   return (
-    <select className={styles.select}>
-      <option>ms</option>
-      <option>s</option>
-    </select>
+    <label>
+      <VisuallyHidden>Select the time unit:</VisuallyHidden>
+
+      <select className={styles.select}>
+        <option value="ms">ms</option>
+        <option value="s">s</option>
+      </select>
+    </label>
   );
 };
