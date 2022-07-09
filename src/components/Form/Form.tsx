f8b9c8628f3dcd3cb3822538b0e73abc7735b869
Alexander Bespoyasov <forjob159@gmail.com>
2022-07-09 13:04:05 +0200

    Use `Input` in the `Form` component

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,4 +1,5 @@
 import { FormEvent } from "react";
+import { Input } from "../Input";
 
 const ignore = (e: FormEvent) => {
   e.preventDefault();
@@ -7,8 +8,9 @@ const ignore = (e: FormEvent) => {
 export const Form = () => {
   return (
     <form onSubmit={ignore}>
-      <input type="date" />
-      <input type="number" />
+      <Input label="Date" type="date" />
+      <Input label="Time Stamp" type="number" />
+
       <select>
         <option>ms</option>
         <option>s</option>
