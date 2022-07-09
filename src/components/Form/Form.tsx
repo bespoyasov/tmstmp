56991e59e77e8f7d7678fd97f2f0d2d26b3c3700
Alexander Bespoyasov <forjob159@gmail.com>
2022-07-09 18:59:04 +0200

    Connect application to the UI

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,4 +1,9 @@
 import type { FormEvent } from "react";
+import {
+  useUiRepresentation,
+  updateDate,
+  updateStamp,
+} from "../../application";
 
 import { Input } from "../Input";
 import { Select } from "../Select";
@@ -9,10 +14,24 @@ const ignore = (e: FormEvent) => {
 };
 
 export const Form = () => {
+  const { stamp, date } = useUiRepresentation();
+
   return (
     <form className={styles.form} onSubmit={ignore}>
-      <Input label="Date" name="date" type="date" />
-      <Input label="Time Stamp" name="timestamp" type="number" />
+      <Input
+        label="Date"
+        name="date"
+        type="date"
+        value={date}
+        onChange={updateDate}
+      />
+      <Input
+        label="Time Stamp"
+        name="timestamp"
+        type="number"
+        value={stamp}
+        onChange={updateStamp}
+      />
 
       <div className={styles.select}>
         <Select />
--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -1,13 +1,16 @@
 import { VisuallyHidden } from "../VisuallyHidden";
+import { updateUnit, useConverter } from "../../application";
 import { options } from "./options";
 import styles from "./Select.module.css";
 
 export const Select = () => {
+  const { unit } = useConverter();
+
   return (
     <label>
       <VisuallyHidden>Select the time unit:</VisuallyHidden>
 
-      <select className={styles.select}>
+      <select className={styles.select} value={unit} onChange={updateUnit}>
         {options.map(({ label, value }) => (
           <option key={value} value={value}>
             {label}
