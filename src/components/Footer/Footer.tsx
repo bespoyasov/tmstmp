d9b8d1649e761e44d267c50f17785765f469c557
Alexander Bespoyasov <forjob159@gmail.com>
2022-07-09 12:49:28 +0200

    Create a footer component

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,9 @@
+import styles from "./Footer.module.css";
+
+export const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <a href="https://github.com/bespoyasov/tmstmp">GitHub</a>
+    </footer>
+  );
+};
